2d3a3b62f1a55f6a3a11b33dec1d19f5b34396fd
joydeeprony89 <joydeeprony89@gmail.com>
2023-01-03 10:39:01 +0530

    Update match.sql

--- match.sql
+++ match.sql
@@ -1,4 +1,7 @@
- ;WITH cte1 as 
+ ******************************************************************************************************************************
+Using Sql -
+***********
+;WITH cte1 as 
          (SELECT P.group_id,P.Player_ID,COALESCE(M.first_score,0) AS Score FROM matches M
          RIGHT JOIN Players P ON M.first_player = P.Player_id
          UNION ALL
@@ -8,7 +11,22 @@
        (SELECT Group_ID,Player_ID,SUM(Score) AS Score,ROW_NUmber() OVER (Partition BY group_id Order by suM(score) desc,player_id) AS RNM 
         FROM cte1
         GROUP BY GROUP_ID, PLAYER_ID)
-    
  SELECT GROUP_iD,Player_ID AS Winner_ID FROM cte2
  WHERE rnm=1
 
+ ******************************************************************************************************************************
+Using SqlLite -
+**************
+ ;WITH cte1 as 
+         (SELECT P.group_id,P.Player_ID,COALESCE(M.first_score,0) AS Score FROM matches M
+         LEFT JOIN Players P ON M.first_player = P.Player_id
+         UNION ALL
+         SELECT P.group_id,P.Player_ID,COALESCE(M.Second_score,0) AS Score FROM Players P
+         LEFT JOIN  matches M ON M.Second_player = P.Player_id)
+ ,cte2 as
+       (SELECT Group_ID,Player_ID,SUM(Score) AS Score,ROW_NUmber() OVER (Partition BY group_id Order by suM(score) desc,player_id) AS RNM 
+        FROM cte1
+        GROUP BY GROUP_ID, PLAYER_ID)
+    
+ SELECT GROUP_iD,Player_ID AS Winner_ID FROM cte2
+ WHERE rnm=1
